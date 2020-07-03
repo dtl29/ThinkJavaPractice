f5afad4f2e38d2e088dca053270e79eba13e0d42
Daniel Louis <44950643+dtl29@users.noreply.github.com>
2020-07-03 16:55:14 -0400

    Create HelloWorld.java

---
+++ Chapter1/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+This is the first chapter of the book Think Java practice 
+this is a simple hello world program 
+*/
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		//this is a comment to practice with 
+		System.out.println("Hello World!");
+		System.out.println("How are you?");//this is also a comment to practice with
+	}
+}
