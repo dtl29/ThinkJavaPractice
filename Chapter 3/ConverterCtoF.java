e3ff363d1aa74fdb73eab41122556ce491be7820
Daniel Louis <44950643+dtl29@users.noreply.github.com>
2020-07-09 14:42:20 -0400

    Add files via upload

--- Chapter 3/ConverterCtoF.java
+++ Chapter 3/ConverterCtoF.java
@@ -1,1 +1,15 @@
-
+import java.util.Scanner;
+public class ConverterCtoF
+{
+	public static void main(String[] args)
+	{
+		double input;
+		double output;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a temprature to convert: ");
+		input = in.nextDouble();
+		//in.nextline();
+		output = input * (9.0/5.0) + 32;
+		System.out.printf("This is %.1fF", output);
+	}
+}
