d8e0ee823c07db3ad386d1804958575bdde7ec20
Daniel Louis <44950643+dtl29@users.noreply.github.com>
2020-07-10 16:23:37 -0400

    Add files via upload

--- Chapter4/Date.java
+++ Chapter4/Date.java
@@ -1,1 +1,29 @@
-
+public class Date
+{
+    public static void printAmerican(String day, int date, String month, int year)
+    {
+        System.out.println("American Format:");
+	    System.out.print(day);
+	    System.out.print(", ");
+	    System.out.print(month);
+	    System.out.print(", ");
+	    System.out.print(date);
+	    System.out.print(", ");
+	    System.out.println(year);
+    }
+    public static void printEuropean(String day, int date, String month, int year)
+    {
+        System.out.println("European Format:");
+	    System.out.println(day + ", " + date + ", " + month + ", " + year);
+    }
+	public static void main(String[] args)
+	{
+	    String day = "Sunday";
+	    int date = 5;
+	    int year = 2020;
+	    String month = "July"; 
+	    
+	    printAmerican(day, date, month, year);
+	    printEuropean(day, date, month, year);
+	}
+}
